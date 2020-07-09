da722a67b514ad47bfcd6063f8c0c7eb6c8fad38
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-07-08 20:44:35 -0500

    sherlock anagram

--- Promblems/SherlockAnagram.cpp
+++ Promblems/SherlockAnagram.cpp
@@ -1,42 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void getcharSet(string sbtr, set<char> &s)
-{
-    cout<<"word > "<<sbtr<<endl;
-    for (int i = 0; i < sbtr.size(); i++){
-        s.insert(sbtr[i]);
-        cout<<sbtr[i]<<" ";
-
-    }
+vector<char> getStringVec(string s){
+    vector<char> v;
+    for(int i=0;i<s.length(); i++)
+        v.push_back((char)s[i]);    
+    // for(auto x : v)
+    //     cout<<x<<" ";
+    // cout<<endl;
+    return v;
+    
 }
-int sherlockAnagram(string word)
-{
-    map<set<char>, int> dict;
+
+
+// Complete the sherlockAndAnagrams function below.
+int sherlockAndAnagrams(string word) {
+    map<vector<char>, int> dict;
     int c = 0;
     for (int i = 0; i < word.length(); i++)
     {
         for (int j = 1; j <= word.length() - i; j++)
         {
-            set<char> s;
-            getcharSet(word.substr(i, j),s);
-            cout<<s.size()<<endl;
-            if (dict[s] != 0)
+            vector<char> k = getStringVec(word.substr(i, j));   
+            sort(k.begin(),k.end());          
+            if (dict[k] != 0)
             {
                 c++;
-                dict[s] = dict[s] + 1;
+                dict[k] = dict[k] + 1;
             }
             else
-            {                
-                dict.insert(make_pair(s, 1));
-            }
+                dict.insert(make_pair(k, 1));
         }
     }
     return c;
+
 }
 
 int main(int argc, char const *argv[])
 {
-   cout<<sherlockAnagram("abba");
+   cout<<sherlockAndAnagrams("abba");
+    // vector<char> a {'b','b','a'};
+    // vector<char> b {'a','b','b'};
+
+    // map<vector<char>,int> map1;
+    // sort(a.begin(), a.end()); 
+    // map1.insert(make_pair(b,100));
+    
+    // cout << "Sorted \n"; 
+    // for (auto x : a) 
+    //     cout << x << " "; 
+
+    // cout<<map1[b]<<endl;
+
+    
+
+
     return 0;
 }
