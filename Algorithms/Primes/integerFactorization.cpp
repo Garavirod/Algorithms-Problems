d5fd65656987bd7468328fc2d8ed8d1653e2b4b1
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-07-20 20:58:39 -0500

    Factorization

--- Algorithms/Primes/integerFactorization.cpp
+++ Algorithms/Primes/integerFactorization.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int C[10000001];
+int N = 10000000;
+
 /*
     Cada iteracion de i c[i] es una factor primo que divide al numero c[j]
     Ese numero se supondrá como el primo máximo que divide a c[j]
@@ -8,39 +11,42 @@ using namespace std;
     divisiro máximio de c[j]
 
 */
-
-void init_Cirba(vector<int> &c, int N){
-    for(int i=2; i*i<=N; i++){
-        if(c[i]==i)
-            for(int j = 2; (j*i)<=N; j++)
-                c[j] = i;
-    }
+void iniciar_criba()
+{
+    for (int i = 0; i <= N; i++)
+        C[i] = i;
+    C[0] = -1;
+    C[1] = -1;
+    for (int i = 2; i * i <= N; i++)
+        if (C[i] == i)
+            for (int j = i + i; j <= N; j += i)
+                C[j] = i;
 }
 
 // Conjunto de factores primos y su multiplicadad para todo x > 1
-
-void prime_Factors(int X, vector<int> &c){
-    if(X<=1) return;
+void fp(int X)
+{
+    if (X <= 1)
+        return;
     int a = 1;
-    int Y = X/c[X];
-    while(c[X]==c[Y]){
+    int Y = X / C[X];
+    while (C[X] == C[Y])
+    {
         a++;
-        Y = Y/c[Y];
+        Y = Y / C[Y];
     }
-    prime_Factors(Y, c);
-    cout<<c[X]<<"^"<<a<<endl;
+    fp(Y);
+    cout << C[X] << " ^ " << a << endl;
 }
+
 
 
 int main(int argc, char const *argv[])
 {
-    int N = 10000;
-    vector<int> c;
-    for(int i=0; i<=N; i++) c.push_back(i);
-    c[0] = c[1] = -1;
-    init_Cirba(c,N+1);
-    int n;
-    while (cin>>n)    
-        prime_Factors(n,c);    
+    int x;
+    iniciar_criba();
+    while (cin >> x)    
+        fp(x);
+    
     return 0;
 }
