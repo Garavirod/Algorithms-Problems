66f07adb4076335dec7cc08ab42dff9316139554
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2022-03-03 14:58:09 -0600

    Was optimized the elif case on algorithm

--- CrackingInterview/array-string/15-one-away.py
+++ CrackingInterview/array-string/15-one-away.py
@@ -13,23 +13,26 @@
 
 def one_away( s1: str, s2: str ) -> bool:
     l1,l2 = list(s1), list(s2)
-    if len(l1) == len(l2): #propbaly was edited trought repleacing
+    length_l1, length_l2 = len(l1),len(l2)
+    if length_l1 == length_l2: #propbaly was edited through repleacing
         editions = 0
-        for i in range(len(l1)):
+        for i in range(length_l1):
             if l1[i] != l2[i]:
                 editions += 1            
         return editions <= 1
      # propbaly was edited through increasing or removing
-    elif abs(len(l1) - len(l2)) == 1:
-        editions = 1                    
-        for s in s2:            
-            if not s in l1:
-                editions += 1
-        return editions <= 1
+    elif abs(length_l1 - length_l2) == 1:          
+        i,j = 0,0            
+        while i < length_l1 and j < length_l2:
+            if s1[i] != s2[j]:
+                return False
+            i +=1 
+            j +=1
+        return True
     else:
         return False
 
 
 if __name__ == '__main__':
-    s1,s2 = "pale", "bae"
+    s1,s2 = "pale", "ble"
     print(one_away(s1,s2))
