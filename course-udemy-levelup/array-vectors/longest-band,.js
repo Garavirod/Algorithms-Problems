d538454f8114921dea7f66fc11f21ebc4ba54ca9
Garavirod <rodrigogarciaavila26@gmail.com>
2022-03-17 18:51:39 -0600

    longest band in o(n)

--- course-udemy-levelup/array-vectors/longest-band,.js
+++ course-udemy-levelup/array-vectors/longest-band,.js
@@ -10,6 +10,36 @@
 
     A longest band  is the band  (subsequence) which contains
     maximum integers.
-
     
 */
+
+
+function longestBand( arr ){
+    let unorderSet = new Set()
+
+    arr.forEach(e => {
+       unorderSet.add(e) 
+    });
+    let largest = 1
+    console.log(unorderSet);
+
+    unorderSet.forEach(e => {
+        let previous = e - 1
+        if( unorderSet.has( previous ) ){
+            let next_no = e + 1
+            let cnt = 2
+            while ( unorderSet.has(next_no) ) {
+                next_no += 1
+                cnt += 1
+            }
+            largest = Math.max(largest,cnt)
+        }
+    })
+
+    return largest
+}
+
+
+const arr = [1,9,3,0,18,5,2,10,7,12,6]
+
+console.log(longestBand( arr ));
