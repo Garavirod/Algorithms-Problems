9236b4ee5b6e7e52db360545dd5a62383d2f0988
Garavirod <rodrigogarciaavila26@gmail.com>
2022-03-28 20:07:19 -0600

    ew problem

---
+++ course-udemy-levelup/strings/search-all.js
@@ -0,0 +1,13 @@
+/* 
+    Implement a function that returns
+    a list of all occurences of a given substring
+    inside a big string
+*/
+
+const searchAll = (big, samll) => {
+    
+} 
+
+
+const big = "I kiked the movie, the movies was great!"
+const small = "movie"
