bec53ac7cf99fdafd7b761ff5bd3be7742b1b29b
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2022-03-31 14:36:31 -0600

    Create camel-case.ts

---
+++ Promblems/hakerrank/typescript/camel-case.ts
@@ -0,0 +1,15 @@
+function camelCase(str: string): number {
+    let counterwords = 0
+    let unorderSet = new Set()
+
+    for (let w = 65; w <= 91; w++)
+        unorderSet.add(String.fromCharCode(w))
+    
+    for (let i = 0; i < str.length; i++) {
+        if(unorderSet.has(str[i])){
+            counterwords++
+        }
+    }
+    counterwords++
+    return counterwords
+}
