a4fa349deb2059bef6390ee63b9804035bc270d6
Garavirod <rodrigogarciaavila26@gmail.com>
2022-04-10 20:10:02 -0500

    ladder problem iteration form

--- course-udemy-levelup/DP/iterative/ladder-problem.js
+++ course-udemy-levelup/DP/iterative/ladder-problem.js
@@ -2,6 +2,23 @@
     Given a ladder of size N, and an integer of K, write 
     function to compute number of ways to climb the ladder
     if you can take a jump of almost k a every step.
+
+    1   stairs with 1,2,3,...k jumps
+    
+    2   stairs with 1,2,3,...k jumps
+    
+    3   stairs with 1,2,3,...k jumps
+    
+    4   stairs with 1,2,3,...k jumps
+
+    it1 = up 1 st with 1,2,3 = 1 - All the ways to up 1 stairs with k jumps 
+    it2 = it1 + up st 2 with(1,2,3)k = 2 - All the ways to up 2 stairs with k jumps 
+    it3 = it1 + it2  + up 3 with (1,2,3)k = 4 - All the ways to up 3 stairs with k jumps 
+    it4 = it1 + it2  + up 4 with (1,2,3)k = 7 - All the ways to up 4 stairs with k jumps 
+
+
+    response = it1 + it2 + it3 it4
+    
 */
 
 let inputData = []
@@ -14,7 +31,7 @@ const ladderProblem = (k, n) => {
     table[0] = 1
 
     for (let i = 1; i <= n; i++) {
-        for (let j = 0; j <= k; j++) {
+        for (let j = 1; j <= k; j++) {
             if (i - j >= 0) table[i] += table[i - j]
         }
     }
@@ -24,7 +41,7 @@ const ladderProblem = (k, n) => {
 }
 
 
-/* process.stdin.on("data", (data) => {
+process.stdin.on("data", (data) => {
     inputData.push( data.toString().trim() )
 })
 
@@ -37,6 +54,6 @@ const main = () => {
     const k = readLine()
     const n = readLine()
     console.log( ladderProblem(k,n) );
-} */
+}
 
-console.log( ladderProblem(3,4) );
+/* console.log( ladderProblem(3,4) ); */
