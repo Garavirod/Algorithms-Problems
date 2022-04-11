7c61353bb14b076f5bf5af856bea672ba0f01342
Garavirod <rodrigogarciaavila26@gmail.com>
2022-04-11 13:58:02 -0500

    tabulation min coin that i need to get i sum until target sum

--- course-udemy-levelup/DP/recursion/coin-change.js
+++ course-udemy-levelup/DP/recursion/coin-change.js
@@ -1,8 +1,29 @@
 /* 
-    Given an arrayof coin denominations,
+    Given an array of coin denominations,
     and integer M representing target money.
     We need to finds the min coins required
     to make the change.
     NOTE: You can take the same coin as you want
 */
 
+const coinChange = (targetMoney, coins, memo={}) => {
+    if (targetMoney in memo) return memo[targetMoney]
+    if (targetMoney === 0) return []
+    if (targetMoney < 0 ) return null    
+    let minChange = null
+    for(let c of coins){        
+        let change = coinChange(targetMoney - c, coins, memo)
+        if( change !== null ){
+            change = [...change, c]
+            if ( !minChange || minChange.length > change.length )
+                minChange = change
+        }
+    }
+    memo[targetMoney] = minChange
+    return minChange
+}
+
+const targetMoney = 1000
+const coins = [1,2,3,15]
+
+console.log( coinChange(targetMoney, coins) );
