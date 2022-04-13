bd775cb306b8cfb97ebb5a5af38b72ecdfbb14ca
Garavirod <rodrigogarciaavila26@gmail.com>
2022-04-13 14:16:06 -0500

    memorrization

--- course-udemy-levelup/DP/recursion/rods-cutting.js
+++ course-udemy-levelup/DP/recursion/rods-cutting.js
@@ -1,12 +1,13 @@
-const rodsCutting = (n, lengthsPrices) => {
+const rodsCutting = (n, lengthsPrices, memo = {}) => {
+    if (n in memo) return memo[n]
     if (n <= 0) return 0
     let maxProfit = 0
     for (let i = 1; i <= n; i++) {
-        let profitsum = lengthsPrices[i - 1] + rodsCutting(n - i, lengthsPrices)
+        let profitsum = lengthsPrices[i - 1] + rodsCutting(n - i, lengthsPrices, memo)
         maxProfit = Math.max(maxProfit, profitsum)
     }
-
+    memo[n] = maxProfit
     return maxProfit
 }
 
-console.log(rodsCutting(8,[1,5,8,9,10,17,17,20]));
+console.log(rodsCutting(20,[1,5,8,9,10,17,17,20,2,13,24,23,120,3,5,6,7,89,900,144]));
