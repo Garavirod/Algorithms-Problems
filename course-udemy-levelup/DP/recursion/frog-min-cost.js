06e823891062ac76e83e2ff4ac28a135b8951f96
Garavirod <rodrigogarciaavila26@gmail.com>
2022-04-19 13:52:12 -0500

    frog min cost was solved using memorization

--- course-udemy-levelup/DP/recursion/frog-min-cost.js
+++ course-udemy-levelup/DP/recursion/frog-min-cost.js
@@ -6,7 +6,7 @@
     Frog will reapeat the following action some
     number of times to reach stone N:
 
-        -if the frog is currently on stone i
+        if the frog is currently on stone i
         jump to stone i+1 or jump to i+2, here a
         cost of |h1-hj| is incurred, where j is the
         stone to land on.
@@ -15,17 +15,20 @@
     before the frog reaches stone NN.
 */
 
-const minFrogCost = (i, stones) => {    
-    if (i === stones.length - 1) return stones[i]
-    if (i > stones.length - 1) return Number.MAX_SAFE_INTEGER
+const minFrogCost = (i, stones, memo = {}) => {
+    if (i in memo) return memo[i]
+    if (i === stones.length - 2) return Math.abs(stones[i] - stones[i + 1])
+    if (i === stones.length - 3) {
+        return Math.min(
+            Math.abs(stones[i] - stones[i + 1]),
+            Math.abs(stones[i] - stones[i + 2]),
+        )
+    }
+    let a = Math.abs(stones[i] - stones[i + 1]) + minFrogCost(i + 1, stones, memo)
+    let b = Math.abs(stones[i] - stones[i + 2]) + minFrogCost(i + 2, stones, memo)
 
-    let minCost = 0
-    const a = Math.abs(stones[i] - minFrogCost(i + 1, stones))
-    const b = Math.abs(stones[i] - minFrogCost(i + 2, stones))
-    console.log(a);
-    console.log(b);
-    minCost += Math.min(a, b)
-    return minCost
+    memo[i] = Math.min(a, b)
+    return memo[i]
 }
 
 const stones = [30, 10, 60, 10, 60, 50]
