2d86925dcd6c2c18b46fcaea876d80f1b985ad70
Garavirod <rodrigogarciaavila26@gmail.com>
2022-04-23 14:34:58 -0500

    LIS solved using recursion and memorization

--- course-udemy-levelup/DP/recursion/longest-increasing-sub.js
+++ course-udemy-levelup/DP/recursion/longest-increasing-sub.js
@@ -4,21 +4,28 @@
     of the subsecuence are sorted in increasing order.
 */
 
+const lis = (i, arr, n, prev, memo = {}) => {
 
+    //Memorization
+    if (i in memo) return memo[i]
 
+    // Base case if nothing remaining
+    if (i == n) return 0
 
-const lis = (i, arr) => {
-    if (i === arr.length - 1) return 0
+    // Exclude the current element i and preoces
+    // the remaining
+    let exclude = lis(i + 1, arr, n, prev, memo)
 
-    let maxSub = 1
-    
-    for (let j = i; j <= arr.length-1; j++) {
-        let resp = 1 + lis(j+1, arr)        
-        if (arr[j] > arr[i]) {            
-            maxSub += resp
-        }
-    }
-    return maxSub
+    //Inlcuding current element if it is grater
+    //than previous element
+    let include = 0
+    if (arr[i] > prev)
+        include = 1 + lis(i + 1, arr, n, arr[i], memo)
+
+    // retunr max above two choices
+    return Math.max(include, exclude)
 }
 
-console.log(lis(0, [50, 4, 10, 8, 30, 100]));
+const arr = [50, 4, 10, 8, 30, 100]
+
+console.log(lis(0, arr, arr.length, Number.MIN_SAFE_INTEGER));
