3e07b1981a6a8ff19f11b5ab6b055fbf67981468
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2022-04-28 11:25:44 -0500

    Update missing-number.js

--- leetcode-patterns/missing-number.js
+++ leetcode-patterns/missing-number.js
@@ -1,5 +1,9 @@
 /* 
     Missign number
+    
+    
+    Given an array nums containing n distinct numbers in the range [0, n], 
+    return the only number in the range that is missing from the array.
 
     Using Gauss formula
 
