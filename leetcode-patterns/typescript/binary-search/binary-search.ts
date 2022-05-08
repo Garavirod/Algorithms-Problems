692506e298140279201c5dc3936a4764be824778
Garavirod <rodrigogarciaavila26@gmail.com>
2022-05-08 11:13:28 -0500

    binary search

---
+++ leetcode-patterns/typescript/binary-search/binary-search.ts
@@ -0,0 +1,14 @@
+function search(nums:number[], target:number):number{
+    let l = 0, r = nums.length - 1
+    while(l<=r){
+        const m = Math.floor((l+r)/2)
+        if(nums[m] > target){
+            r = m - 1
+        }else if(nums[m] < target){
+            l = m + 1
+        }else{
+            return m
+        }
+    }
+    return -1
+}
