3794458a789dc13101ccd1ae177f978fa5b92627
Garavirod <rodrigogarciaavila26@gmail.com>
2022-05-09 20:45:08 -0500

    invert tree

---
+++ leetcode-patterns/typescript/trees/invert-tree.ts
@@ -0,0 +1,26 @@
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+ function invertTree(root: TreeNode | null): TreeNode | null {
+    if(!root) return root
+
+    //swap nodes pointers
+    const temp = root.right
+    root.right = root.left
+    root.left = temp
+    // swap pointers the remaining nodes (left, right)
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
+
+};
