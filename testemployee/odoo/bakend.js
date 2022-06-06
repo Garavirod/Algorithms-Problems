c3074ea68a4585663b60f9b0dd9de57612c9be7f
Garavirod <rodrigogarciaavila26@gmail.com>
2022-06-05 20:58:34 -0500

    frontend finished

--- testemployee/odoo/bakend.js
+++ testemployee/odoo/bakend.js
@@ -1,7 +1,7 @@
 
 
-const question2 = () => {
-    
+const question2 = (array) => {
+    return array.sort(() => Math.random() - 0.5);
 }
 
 
@@ -12,15 +12,16 @@ const question2 = () => {
  */
 const question5 = (str) => {
     /* Regexp rule */
-    const reg = /[a-z]{4,9})\w*/gi
+    const reg = /\w{0,5}(odoo|work|code)\w{0,5}/gi
     return str.match(reg) 
 }
 
 
 const main = () => {
+    console.log("-----Question 2----");
+    console.log(question2(['A','1','2','3','4','5','6','7','8','9','10','J','K','Q']));
     console.log("-----Question 5----");
-    console.log(question5("I lovework in odoo because is great"));
-    
+    console.log(question5("I love work in odoo because is great to code on letcodeqwe in his office"));    
 }
 
 main();
