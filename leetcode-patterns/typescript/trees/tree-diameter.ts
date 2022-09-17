b55d6a8b8b120bf481e2b5052b5231a13473e405
Garavirod <rodrigogarciaavila26@gmail.com>
2022-09-17 13:32:53 -0500

    Diameter of binary tree

---
+++ leetcode-patterns/typescript/trees/tree-diameter.ts
@@ -0,0 +1,29 @@
+
+// Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+
+
+function diameterOfBinaryTree(root: TreeNode | null): number {
+    var diameter = 0;
+    function dfs(root: TreeNode | null) {
+        if (!root) return -1;
+    
+        let left = dfs(root.left);
+        let right = dfs(root.right);
+        diameter = Math.max(diameter, 2 + left + right);
+        return 1 + Math.max(left, right);
+    }
+    dfs(root);
+    return diameter;
+};
