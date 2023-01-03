7dcdd30c7d843c1b83e10c269eb0d0271f431b1a
Garavirod <rodrigogarciaavila26@gmail.com>
2023-01-03 16:04:15 -0600

    39 leetcode solution backtracking approach

---
+++ leetcode-patterns-solutions/backtracking/combinationSum.ts
@@ -0,0 +1,23 @@
+/* 
+    39. Combination Sum
+*/
+function combinationSum(candidates: number[], target: number): number[][] {
+    let results: number[][] = [];
+    const dfs = (i: number, combination: number[], total: number): any => {
+        if (total === target) {
+            results.push([...combination]);
+            return;
+        }
+        // Not posible solution
+        if (i >= candidates.length || total > target) return;
+
+        // Include i as part of combination
+        combination.push(candidates[i]);
+        dfs(i, combination, total + candidates[i]);
+        //i is NOT include it as part of combination
+        combination.pop();
+        dfs(i + 1, combination, total);
+    }
+    dfs(0,[],0);
+    return results;
+};
