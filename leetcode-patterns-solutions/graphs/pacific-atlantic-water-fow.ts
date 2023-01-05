511cbcf60c4e1d6202c4537f49f2333e3c666bd3
Garavirod <rodrigogarciaavila26@gmail.com>
2023-01-05 13:52:40 -0600

    Fix 417 Leetcode solution BFS approach

--- leetcode-patterns-solutions/graphs/pacific-atlantic-water-fow.ts
+++ leetcode-patterns-solutions/graphs/pacific-atlantic-water-fow.ts
@@ -1,62 +1,76 @@
 /* 417. Pacific Atlantic Water Flow */
 
-function pacificAtlantic(heights: number[][]): number[][] {
-    const ROW = heights.length, COL = heights[0].length;
+/**
+ * Breath First Search implementation
+ * @param matrix 
+ * @param queue 
+ * @param visited 
+ */
+const bfs = (matrix: number[][], queue: number[][], visited: boolean[][]): void => {
+    let m = matrix.length, n = matrix[0].length;
+    let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
-    let pacific: boolean[][] = Array.from({ length: heights.length }, () => Array.from({length:heights[0].length}, () => false));
-    let atlantic: boolean[][] = Array.from({ length: heights.length }, () => Array.from({length:heights[0].length}, () => false));
-    let pq: number[][] = [];
-    let aq: number[][] = [];
-    let res: number[][] = [];
-
-    for (let i = 0; i < ROW; i++) {
-        pacific[i][0] = true;
-        atlantic[COL - 1][i] = true;
-        aq.push([COL - 1, i]);
-        pq.push([i, 0]);
-    }
-
-    for (let j = 0; j < COL; j++) {
-        pacific[0][j] = true;
-        atlantic[ROW - 1][j] = true;
-        aq.push([ROW - 1, j]);
-        pq.push([0, j]);
-    }
-
-    const bfs = (matrix: number[][], queue: number[][], visited: boolean[][]) => {
-        let n = matrix.length, m = matrix[0].length;
-        let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
-        while (queue.length > 0) {
-            const u = queue.shift()!;
-            for (let dir of directions) {
-                const x = u[0] + dir[0], y = u[1] + dir[1];
-                if (x < 0 ||
-                    y < 0 ||
-                    x >= n ||
-                    y >= m ||
-                    visited[x][y] ||
-                    matrix[x][y] < matrix[u[0]][u[1]]
-                ) continue;
-
+    while (queue.length > 0) {
+        const [qCordx, qCordy] = queue.shift()!;
+        for (let dir of directions) {
+            const x = qCordx + dir[0], y = qCordy + dir[1];
+            if (!(
+                x < 0 ||
+                y < 0 ||
+                x >= m ||
+                y >= n ||
+                visited[x][y] ||
+                matrix[x][y] < matrix[qCordx][qCordy])
+            ) {
                 visited[x][y] = true;
                 queue.push([x, y]);
             }
+
         }
+    }
+}
+
+/**
+ * Creates a Matrix NXM with false values
+ */
+const createMatrix = (n: number, m: number): boolean[][] =>
+    Array.from({ length: n }, () => Array.from({ length: m }, () => false));
+
+
+function pacificAtlantic(heights: number[][]): number[][] {
+    const ROWS = heights.length, COLS = heights[0].length;
+
+    let pacific: boolean[][] = createMatrix(ROWS, COLS);
+    let atlantic: boolean[][] = createMatrix(ROWS, COLS);
+    let pacQueue: number[][] = [];
+    let atlQueue: number[][] = [];
+    let results: number[][] = [];
+
+    // Set as true Pacific edges
+    for (let i = 0; i < ROWS; i++) {
+        pacific[i][0] = true;
+        atlantic[i][COLS - 1] = true;
+        atlQueue.push([i, COLS - 1]);
+        pacQueue.push([i, 0]);
+    }
+    // Set as true atlantic edges
+    for (let j = 0; j < COLS; j++) {
+        pacific[0][j] = true;
+        atlantic[ROWS - 1][j] = true;
+        atlQueue.push([ROWS - 1, j]);
+        pacQueue.push([0, j]);
     }
 
     // BFS
-    bfs(heights, pq, pacific);
-    bfs(heights, aq, atlantic);
+    bfs(heights, pacQueue, pacific);
+    bfs(heights, atlQueue, atlantic);
 
     // Verify intersections
-    for (let i = 0; i < ROW; i++) {
-        for (let j = 0; j < COL; j++) {
-            if (pacific[i][j] === atlantic[i][j]) res.push([i, j]);
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            if (pacific[i][j] && atlantic[i][j]) results.push([i, j]);
         }
     }
 
-    return res;
+    return results;
 };
-
-const heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
-console.log(pacificAtlantic(heights))
