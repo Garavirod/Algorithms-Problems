a9e032743673e416dfbe0571408fed877148df14
Garavirod <rodrigogarciaavila26@gmail.com>
2023-01-08 16:14:40 -0600

    1143 Leetcode solution Longest common subsequence

--- leetcode-patterns-solutions/dynamic-program/2d/1143-longest-common-subsequence.ts
+++ leetcode-patterns-solutions/dynamic-program/2d/1143-longest-common-subsequence.ts
@@ -2,18 +2,19 @@ function createMatrix(m:number, n:number): number[][]{
     return Array.from({length:m}, ()=> Array.from({length:n}, () => 0));
 }
 
-function lcs(w1:string, w2:string):number{
-    const m = w1.length + 1, n = w2.length + 1;
+function longestCommonSubsequence(text1: string, text2: string):number{
+    const m = text1.length + 1, n = text2.length + 1;
     let matrix = createMatrix(m, n);
 
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
-            if(w2[i-1] === w1[j-1]){
-                matrix[i][j] = Math.max();
+            if(text1[i-1] === text2[j-1]){
+                matrix[i][j] = 1 + matrix[i-1][j-1];
+            }else{
+                const topCell = matrix[i-1][j], rigghtCell = matrix[i][j-1];
+                matrix[i][j] = Math.max(topCell,rigghtCell);
             }
         }
     }
-
     return matrix[m-1][n-1];
-
 }
