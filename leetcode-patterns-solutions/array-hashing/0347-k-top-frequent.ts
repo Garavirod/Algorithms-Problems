e05afca416435be9c0b4b3d90638feb969321757
Garavirod <rodrigogarciaavila26@gmail.com>
2023-02-06 17:46:23 -0600

    0347 Leetcode solution 'bucket sort' and 'hash map' approach

--- leetcode-patterns-solutions/array-hashing/0347-k-top-frequent.ts
+++ leetcode-patterns-solutions/array-hashing/0347-k-top-frequent.ts
@@ -1,19 +1,17 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    let result:number[] = [];
+    let result: number[] = [];
     let charCounter = new Map<number, number>();
-    let bucketSort = Array.from({length: nums.length + 1}, 
+    let bucketSort = Array.from({ length: nums.length + 1 },
         () => new Array<number>());
 
-    for(const n of nums){
-        if(!charCounter.has(n)) charCounter.set(n, 1);
-        else charCounter.set(n, charCounter.get(n)! + 1);
-    }
+    for (const n of nums)
+        charCounter.set(n, (charCounter.get(n) || 0) + 1)
 
-    for(const [num, times] of charCounter)
+    for (const [num, times] of charCounter)
         bucketSort[times].push(num);
-    
+
     for (let i = bucketSort.length - 1; i >= 0; i--) {
-        for (const n of bucketSort[i]){
+        for (const n of bucketSort[i]) {
             result.push(n);
             if (result.length === k)
                 return result;
